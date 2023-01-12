5a5023c753fa08bcd48d1279081e08ac44079f79
ThangVu162 <56299002+ThangVu162@users.noreply.github.com>
2023-01-13 00:38:14 +0700

    add enemy, player plane

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,14 +12,20 @@ public class GameController : MonoBehaviour
     [SerializeField] private Canvas mainCanvas;
     [SerializeField] private GameObject enemiesLayout;
     [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private Transform startPoint;
+    [SerializeField] private List<Transform> listPointInPath;
 
+    private List<Enemies> enemies = new List<Enemies>();
     private RectTransform mainCanvasRecttransform;
     private float speedMoveBackground = 50;
     private int totalEnemies = 12;
+
     void Start()
     {
         mainCanvasRecttransform = mainCanvas.GetComponent<RectTransform>();
         PlayBackgroundEffect();
+
+        SpawnEnemies();
     }
 
     private void PlayBackgroundEffect()
@@ -26,5 +33,71 @@ public class GameController : MonoBehaviour
         var distanceMove = mainCanvasRecttransform.sizeDelta.y;
         LeanTween.moveLocalY(background_1.gameObject, -mainCanvasRecttransform.sizeDelta.y, distanceMove / speedMoveBackground).setRepeat(Int32.MaxValue).setOnCompleteOnRepeat(true);
         LeanTween.moveLocalY(background_2.gameObject, 0, distanceMove / speedMoveBackground).setRepeat(Int32.MaxValue).setOnCompleteOnRepeat(true);
+    }
+
+    private async void SpawnEnemies()
+    {
+        for(int i = 0; i < totalEnemies; i++)
+        {
+            var enemyContainer = Instantiate(enemyPrefab, enemiesLayout.transform);
+            var enemyScript = enemyContainer.transform.Find("EnemySprite")?.GetComponent<Enemies>();
+
+            enemies.Add(enemyScript);
+        }
+
+
+        for(int i = totalEnemies - 1; i >= 0; i--)
+        {
+            enemies[i].transform.position = startPoint.position;
+            enemies[i].gameObject.SetActive(true);
+
+            EnemyMoveInPath(enemies[i]);
+            await Task.Delay(1000);
+        }
+    }
+
+    private async void EnemyMoveInPath(Enemies enemy)
+    {
+        var speed = 2f;
+        var distance = Vector2.Distance(startPoint.position, listPointInPath[0].position);
+        float timeMoveBetweenTwoPoint = distance / speed;
+
+        LeanTween.move(enemy.gameObject, listPointInPath[0].position, timeMoveBetweenTwoPoint);
+        await Task.Delay((int)(timeMoveBetweenTwoPoint * 1000));
+
+        distance = Vector2.Distance(listPointInPath[0].position, listPointInPath[1].position);
+        timeMoveBetweenTwoPoint = distance / speed;
+        LeanTween.move(enemy.gameObject, listPointInPath[1].position, timeMoveBetweenTwoPoint);
+        await Task.Delay((int)(timeMoveBetweenTwoPoint * 1000));
+
+        distance = Vector2.Distance(listPointInPath[1].position, listPointInPath[2].position);
+        timeMoveBetweenTwoPoint = distance / speed;
+        LeanTween.move(enemy.gameObject, listPointInPath[2].position, timeMoveBetweenTwoPoint);
+        await Task.Delay((int)(timeMoveBetweenTwoPoint * 1000));
+
+        distance = Vector2.Distance(listPointInPath[2].position, listPointInPath[3].position);
+        timeMoveBetweenTwoPoint = distance / speed;
+        LeanTween.move(enemy.gameObject, listPointInPath[3].position, timeMoveBetweenTwoPoint);
+        await Task.Delay((int)(timeMoveBetweenTwoPoint * 1000));
+
+        distance = Vector2.Distance(listPointInPath[3].position, listPointInPath[4].position);
+        timeMoveBetweenTwoPoint = distance / speed;
+        LeanTween.move(enemy.gameObject, listPointInPath[4].position, timeMoveBetweenTwoPoint);
+        await Task.Delay((int)(timeMoveBetweenTwoPoint * 1000));
+
+        distance = Vector2.Distance(listPointInPath[4].position, listPointInPath[5].position);
+        timeMoveBetweenTwoPoint = distance / speed;
+        LeanTween.move(enemy.gameObject, listPointInPath[5].position, timeMoveBetweenTwoPoint);
+        await Task.Delay((int)(timeMoveBetweenTwoPoint * 1000));
+
+        distance = Vector2.Distance(listPointInPath[5].position, listPointInPath[0].position);
+        timeMoveBetweenTwoPoint = distance / speed;
+        LeanTween.move(enemy.gameObject, listPointInPath[0].position, timeMoveBetweenTwoPoint);
+        await Task.Delay((int)(timeMoveBetweenTwoPoint * 1000));
+
+        var endPoint = enemy.transform.parent.TransformPoint(Vector2.zero);
+        distance = Vector2.Distance(enemy.transform.position, endPoint);
+        
+        LeanTween.moveLocal(enemy.gameObject, Vector2.zero, distance / speed);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private List<Transform> listEndPointOfBullet;
     void Start()
     {
         
